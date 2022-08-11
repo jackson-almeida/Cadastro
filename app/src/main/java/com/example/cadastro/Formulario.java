a1edabd4cb37b58e5bc837d3d41379590a0979c2
jackson <jackson@debian>
2022-08-10 21:43:37 -0300

    Alterado nomes dos ids

---
+++ app/src/main/java/com/example/cadastro/Formulario.java
@@ -0,0 +1,4 @@
+package com.example.cadastro;
+
+public class Formulario {
+}
--- app/src/main/java/com/example/cadastro/MainActivity.java
+++ app/src/main/java/com/example/cadastro/MainActivity.java
@@ -4,11 +4,18 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.example.cadastro.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+    private Formulario form;
+    private ActivityMainBinding acb;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+//        acb.buttonLimpar.
     }
 }
