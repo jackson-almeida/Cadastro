71ebf39b097688b82fea19428f3674b6bd843551
jackson <jackson@debian>
2022-08-17 19:06:32 -0300

    Implementado integração dos campos com a tela

--- app/src/main/java/com/example/cadastro/Formulario.java
+++ app/src/main/java/com/example/cadastro/Formulario.java
@@ -1,4 +1,93 @@
 package com.example.cadastro;
 
 public class Formulario {
+    private String nome;
+    private String telefone;
+    private String email;
+    private boolean ingressar;
+    private String sexo;
+    private String cidade;
+    private String UF;
+
+    public Formulario() {
+    }
+
+    public Formulario(String nome, String telefone, String email, boolean ingressar, String sexo, String cidade, String UF) {
+        this.nome = nome;
+        this.telefone = telefone;
+        this.email = email;
+        this.ingressar = ingressar;
+        this.sexo = sexo;
+        this.cidade = cidade;
+        this.UF = UF;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public boolean isIngressar() {
+        return ingressar;
+    }
+
+    public void setIngressar(boolean ingressar) {
+        this.ingressar = ingressar;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getUF() {
+        return UF;
+    }
+
+    public void setUF(String UF) {
+        this.UF = UF;
+    }
+
+    @Override
+    public String toString() {
+        return "Formulario{" +
+                "nome='" + nome + '\'' +
+                ", telefone='" + telefone + '\'' +
+                ", email='" + email + '\'' +
+                ", ingressar=" + ingressar +
+                ", sexo='" + sexo + '\'' +
+                ", cidade='" + cidade + '\'' +
+                ", UF='" + UF + '\'' +
+                '}';
+    }
 }
--- app/src/main/java/com/example/cadastro/MainActivity.java
+++ app/src/main/java/com/example/cadastro/MainActivity.java
@@ -3,6 +3,13 @@ package com.example.cadastro;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.cadastro.databinding.ActivityMainBinding;
 
@@ -10,12 +17,56 @@ public class MainActivity extends AppCompatActivity {
     private Formulario form;
     private ActivityMainBinding acb;
 
+    private EditText etName;
+    private EditText etPhone;
+    private EditText etEmail;
+    private CheckBox cbJoin;
+    private RadioButton rbMasc;
+    private RadioButton rbFem;
+    private EditText etCity;
+    private Spinner spUF;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        etName = findViewById(R.id.etName);
+        etPhone = findViewById(R.id.etPhone);
+        etEmail = findViewById(R.id.etEmail);
+        cbJoin = findViewById(R.id.cbJoin);
+        rbMasc = findViewById(R.id.rbMasc);
+        rbFem = findViewById(R.id.rbFem);
+        etCity = findViewById(R.id.etCidade);
+        spUF = findViewById(R.id.spinnerUF);
 
-//        acb.buttonLimpar.
+        acb.buttonLimpar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                etName.setText("");
+                etPhone.setText("");
+                etEmail.setText("");
+                etCity.setText("");
+                cbJoin.setChecked(false);
+                rbMasc.setChecked(false);
+                rbFem.setChecked(false);
+                spUF.setSelection(0);
+            }
+        });
+
+        acb.buttonSalvar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Formulario form = new Formulario();
+                form.setNome(etName.getText().toString());
+                form.setTelefone(etPhone.getText().toString());
+                form.setEmail(etEmail.getText().toString());
+                form.setIngressar(cbJoin.isChecked());
+                form.setSexo(String.valueOf(rbFem.isChecked() ? rbFem.getText() : rbMasc.getText()));
+                form.setCidade(etCity.getText().toString());
+                form.setUF(((TextView) spUF.getSelectedView()).getText().toString());
+                Toast.makeText(MainActivity.this, form.toString(), Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
